dafbd97798b41ec966caaa45e7ac397dd0949022
aluis <andreluis9214@gmail.com>
2017-07-07 14:29:47 -0300

    issue #10 | fetch data from nutritionix api

--- server/app/controllers/setFood.js
+++ server/app/controllers/setFood.js
@@ -1,3 +1,5 @@
+
+const fetchNutritionixAPI = require('../../utils/fetchNutritionixAPI');
 const foodModel = require('../model/foods');
 
 function setFood(req, res) {
@@ -6,11 +8,41 @@ function setFood(req, res) {
         return;
     }
 
-    foodModel.createFood(req.body);
+    fetchNutritionixAPI(
+        req.body.title,
+        (err, httpResponse, body) => {
+            const parsedBody = JSON.parse(body);
+            if (
+                err ||
+                !parsedBody ||
+                !parsedBody.hits
+            ) {
+                res.send('error');
+            }
 
-    res.send({
-        status: 'ok'
-    });
+            try {
+                const moreHealthFood = parsedBody.hits[0].fields;
+
+                foodModel.setFood({
+                    title: moreHealthFood.item_name,
+                    info: {
+                        cholesterol: moreHealthFood.nf_cholesterol,
+                        calories: moreHealthFood.nf_calories,
+                        sodium: moreHealthFood.nf_sodium
+                    }
+                })
+
+                res.send({
+                    status: 'ok'
+                });
+
+            } catch (e) {
+                res.send('error');
+            }
+        }
+    );
+
+    
 }
 
 module.exports = setFood;
--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -8,10 +8,10 @@ const getFoods = require('../controllers/getFoods');
 const setFood = require('../controllers/setFood');
 const removeFood = require('../controllers/removeFood');
 
-
 function routes(app) {
     app.get(`${config.API_URL}/get-all-foods`, getFoods);
     app.post(`${config.API_URL}/add-food`, jsonParser, setFood);
     app.post(`${config.API_URL}/remove-food`, jsonParser, removeFood);
 }
+
 module.exports = routes;
